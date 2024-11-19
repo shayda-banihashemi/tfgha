292447f54a77516cd4f5a04f40e4367eb795219a
Andy Miles <akmiles@icloud.com>
2024-11-18 19:21:22 -0800

    updated server message

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "py_server" {
               app = Flask(__name__)
               @app.route('/')
               def hello():
-                  return "Hello from Python on AWS"
+                  return "Hello from Python on AWS via terraform"
               if __name__ == '__main__':
                   app.run(host='0.0.0.0', port=5000)
               EOL
