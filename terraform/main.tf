44ebc83aece71abc115ed63fdca27fb860929ee6
Andy Miles <akmiles@icloud.com>
2024-12-05 12:43:58 -0800

    test with poetry in User

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
        from_port   = 22
        to_port     = 22
        protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]  # Be cautious with this setting; restrict to specific IPs if possible
+       cidr_blocks = ["0.0.0.0/0"]
      }
 
      egress {
