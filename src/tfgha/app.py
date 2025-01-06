4e505feea2f7c7305d9f650d99121a2fc003f9ea
shayda-banihashemi <shayda.dev@icloud.com>
2025-01-06 16:12:18 -0700

    updated commit

--- src/tfgha/app.py
+++ src/tfgha/app.py
@@ -9,7 +9,7 @@ from chromadb.config import Settings
 from chromadb.utils import embedding_functions
 import functools
 import time
-#comment
+#comment1
 def timer(func):
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
